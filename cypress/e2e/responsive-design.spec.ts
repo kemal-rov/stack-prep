09fdf9847d9f81430e3943a7f04fe36dea290ce1
Kemal <kemo996@gmail.com>
2024-06-22 12:12:00 +0200

    include strongly typed responsive design spec

---
+++ cypress/e2e/responsive-design.spec.ts
@@ -0,0 +1,17 @@
+describe('Responsive Design', () => {
+    const sizes: (Cypress.ViewportPreset | [number, number])[] = ['iphone-6', 'ipad-2', [1024, 768]];
+  
+    sizes.forEach(size => {
+      it(`should display correctly on ${size}`, () => {
+        if (Cypress._.isArray(size)) {
+          cy.viewport(size[0], size[1]);
+        } else {
+          cy.viewport(size as Cypress.ViewportPreset);
+        }
+  
+        cy.visit('/');
+        cy.get('.responsive-element').should('be.visible');
+      });
+    });
+  });
+  
